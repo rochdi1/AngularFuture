65240a3741cf3508206f0a1a8e3d54374007c3e4
rochdi1 <rochdi.abdou3@gmail.com>
2018-11-06 14:24:27 +0100

    v2

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
